7038784309b74671e387f764b8b0aef0156a8287
ymmtmdk <ymmtmdk@gmail.com>
2017-07-12 16:00:30 +0900

    Typing

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,43 +1,47 @@
 <template>
-    <div>
-        <div class="greeting">Hello {{name}}{{exclamationMarks}}</div>
-        <button @click="decrement">-</button>
-        <button @click="increment">+</button>
-    </div>
+  <div>
+    <div class="greeting">Hello {{name}}{{exclamationMarks}}</div>
+    <button @click="decrement">-</button>
+    <button @click="increment">+</button>
+  </div>
 </template>
 
 <script lang="ts">
 
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from 'vue';
+import Component from 'vue-class-component';
 
 @Component({
-    props: {
-      name: String,
-      initialEnthusiasm: Number
-    }
+  props: {
+    name: String,
+    initialEnthusiasm: Number
+  }
 })
-export default class HelloComponent extends Vue {
-    initialEnthusiasm: number
-    enthusiasm = this.initialEnthusiasm
-    increment() {
-      this.enthusiasm++;
-    }
-    decrement() {
-        if (this.enthusiasm > 1) {
-            this.enthusiasm--;
-        }
-    }
 
-    get exclamationMarks(): string {
-        return Array(this.enthusiasm + 1).join('!');
+export default class HelloComponent extends Vue {
+  readonly name: string;
+  readonly initialEnthusiasm: number;
+  private enthusiasm = this.initialEnthusiasm;
+
+  increment() {
+    this.enthusiasm++;
+  }
+
+  decrement() {
+    if (this.enthusiasm > 1) {
+      this.enthusiasm--;
     }
+  }
+
+  get exclamationMarks(): string {
+    return Array(this.enthusiasm + 1).join('!');
+  }
 }
 
 </script>
 
 <style>
 .greeting {
-    font-size: 20px;
+  font-size: 20px;
 }
 </style>
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import Vue from "vue";
 import HelloComponent from "./components/Hello.vue";
 
 const v = new Vue({
-    el: "#app",
-    template: `
+  el: "#app",
+  template: `
     <div>
         Name: <input v-model="name" type="text">
         <hello-component :name="name" :initialEnthusiasm="5" />
     </div>
     `,
-    data: { name: "World" },
-    components: {
-        HelloComponent
-    }
+  data: { name: "World" },
+  components: {
+    HelloComponent
+  }
 });
